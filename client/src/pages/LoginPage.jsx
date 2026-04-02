3fe35b6d7333aeafe5b23756b79ac0168a270b7f
jjacque1 <jacksonjacque.jj@gmail.com>
2026-04-02 15:41:32 -0400

    working on the login form now

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-      <h1>Login page</h1>
+      <form >
+        <h1>Login</h1>
+
+        <input
+          type="text"
+          value={email}
+          placeholder="Enter email"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <input
+          type="text"
+          value={password}
+          placeholder="Enter password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <button>Login</button>
+      </form>
     </>
   );
 }
