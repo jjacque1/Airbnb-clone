d276255a0a99888096cf67872c9694981eb01412
jjacque1 <jacksonjacque.jj@gmail.com>
2026-04-08 15:28:40 -0400

    splitting mono-repo into poly-repo, client will have its own repo and server will have its own repo, deployment will be seperate repos

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -7,10 +7,9 @@ import RegisterPage from "../pages/RegisterPage";
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-
       <Route element={<Layout />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route index element={<HomePage />} />
       </Route>
     </Routes>
